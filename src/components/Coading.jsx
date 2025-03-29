ccb9f26b23558109c81b7d88225c60c218d59a12
MUCHKUNDRAJE <muchkundraje.thote@vectorlab.in>
2025-03-29 20:41:30 +0530

    first commit

---
+++ src/components/Coading.jsx
@@ -0,0 +1,124 @@
+import React from 'react'
+import Editor from '@monaco-editor/react';
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+function Coading() {
+const [Switch, setSwitch] = useState(0);
+const navigate = useNavigate()
+
+
+useEffect(() => {
+  const preventTabSwitch = () => {
+    if (document.hidden) {
+      alert("You cannot switch tabs!");
+      document.title = "Please return to the tab!";
+      window.focus();
+      setSwitch((prev) => prev + 1);
+    } else {
+      document.title = "Coding Challenge";
+    }
+  };
+
+  if (Switch >= 3) {
+    setTimeout(() => {
+      alert("You have changed tabs too many times! You are getting disqualified.");
+      navigate("/");
+    }, 100); 
+  } else if (Switch > 0) {
+    alert(`You have switched tabs ${Switch} times!`);
+  }
+
+  document.addEventListener("visibilitychange", preventTabSwitch);
+
+  return () => {
+    document.removeEventListener("visibilitychange", preventTabSwitch);
+  };
+}, [Switch]);
+
+
+  return (
+    <div className='h-screen w-full bg-[#202124] gap-1  flex overflow-hidden'> 
+    <div className=' w-[50%] bg-[#202124] '>
+    <div className=' w-full  bg-zinc-900 p-5'>
+        <h1 className='text-white text-2xl font-bold '>
+           Compatative Test 
+          </h1> 
+    </div>
+     <div className='h-full w-[100%] p-2 text-white bg-[#202124] text-white'>
+
+     <div class="max-w-2xl  shadow-lg rounded-lg p-6">
+    <h1 class="text-2xl font-bold text-center text-white">344. Reverse String</h1>
+    <p class="mt-4"><strong class="text-white">Difficulty:</strong> <span class="text-green-500">Easy</span></p>
+    <p class="mt-2 text-white">Write a function that reverses a string. The input string is given as an array of characters <code class="bg-zinc-900 px-2 rounded">s</code>.</p>
+    <p class="mt-2 text-white">You must do this by modifying the input array <strong>in-place</strong> with <code class="bg-zinc-900 px-2 rounded">O(1)</code> extra memory.</p>
+
+    <h2 class="mt-6 text-lg font-semibold text-white">Example 1:</h2>
+    <pre class="bg-zinc-900  p-3 rounded-md overflow-x-auto">
+    <strong>Input:</strong> s = ["h","e","l","l","o"]
+    <br />
+    <br />
+     <strong>Output:</strong>s = ["o","l","l","e","h"]
+    </pre>
+
+    <h2 class="mt-4 text-lg font-semibold text-white">Example 2:</h2>
+    <pre class="bg-zinc-900  p-3 rounded-md overflow-x-auto">
+<strong>Input:</strong> s = ["H","a","n","n","a","h"]
+<br />
+<br />
+<strong>Output:</strong> ["h","a","n","n","a","H"]
+    </pre>
+
+    <h2 class="mt-4 text-lg font-semibold text-white">Constraints:</h2>
+    <ul class="list-disc list-inside text-white">
+        <li><code>1 ≤ s.length ≤ 10<sup>5</sup></code></li>
+        <li><code>s[i]</code> is a printable ASCII character.</li>
+    </ul>
+</div>
+
+     </div>
+    </div>
+    
+    <div className='h-full w-[60%] bg-[#202124] border-l-2 border-slate-600 '>
+      <div className='h-10 w-full bg-zinc-800  flex text-white gap-2 items-center px-10 justify-between'>
+        <div className='flex items-center gap-2'>
+         <h1 className='p-2 hover:bg-zinc-700 cursor-pointer  '>Editor</h1>
+         <h2 className='p-2 hover:bg-zinc-700 cursor-pointer '>Discription</h2>
+        </div>
+        <div className='p-2 hover:bg-zinc-700 cursor-pointer text-red-500 font-bold'>Back</div>
+      </div>
+       <button className='bg-white px-2 rounded ml-3 mt-2 mb-2'>Java</button>
+     <Editor 
+          height="50%"
+          defaultLanguage="java"
+          defaultValue={` public class ReverseString {
+                public static void main(String[] args) {
+                 
+        
+                }
+          }`}
+      className=''
+       theme="vs-dark"
+        />
+    <div className=' w-[100%] p-4 text-white flex items-start justify-start flex-col border-t-2 border-slate-600'>
+      
+      <h1 className='text-2xl text-start'>
+          Test Case 
+      </h1>
+      <div className='w-full h-40  flex items-center justify-center'>
+      You must run your code first
+      </div>
+
+    </div>
+    </div>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Coading
---
+++ src/components/Home.jsx
@@ -0,0 +1,100 @@
+import React, { use } from 'react'
+import { useRef, useEffect } from 'react'
+import { useState } from 'react'
+import Webcam from "react-webcam";
+import * as faceapi from "face-api.js";
+
+function Home() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [emotion, setEmotion] = useState("Detecting...");
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const loadModels = async () => {
+      await faceapi.nets.tinyFaceDetector.loadFromUri("model/tiny_face_detector_model-weights_manifest.json");
+      await faceapi.nets.faceExpressionNet.loadFromUri("model/face_expression_model-weights_manifest.json");
+      console.log("Models Loaded from URL ✅");
+    };
+  
+    loadModels();
+  }, []);
+  
+  useEffect(() => {
+    const detectEmotions = async () => {
+      if (
+        webcamRef.current &&
+        webcamRef.current.video.readyState === 4 &&
+        canvasRef.current
+      ) {
+        const video = webcamRef.current.video;
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+  
+        // Match canvas size to video size
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+  
+        const detections = await faceapi
+          .detectSingleFace(video, new faceapi.TinyFaceDetectorOptions())
+          .withFaceExpressions();
+  
+        if (detections) {
+          const expressions = detections.expressions;
+  
+          // Get the emotion with the highest confidence
+          const maxEmotion = Object.keys(expressions).reduce((a, b) =>
+            expressions[a] > expressions[b] ? a : b
+          );
+  
+          setEmotion(`${maxEmotion} (${(expressions[maxEmotion] * 100).toFixed(2)}%)`);
+  
+          // Clear the canvas
+          context.clearRect(0, 0, canvas.width, canvas.height);
+  
+          // Draw detections (bounding box around face)
+          faceapi.draw.drawDetections(canvas, detections);
+  
+          // Draw emotions (text labels for expressions)
+          faceapi.draw.drawFaceExpressions(canvas, detections);
+        }
+      }
+    };
+  
+    const interval = setInterval(detectEmotions, 500);
+    return () => clearInterval(interval);
+  }, []);
+  
+  
+
+
+  return (
+    <div className='h-screen w-full  flex items-center justify-center flex-col gap-1 bg-[#202124]'>
+           
+      <h1 className='text-3xl text-slate-100'>HR Interview</h1>
+
+<div className='flex items-center justify-center gap-4 mt-4'>
+  <div>
+     <h1 className='text-white text-center'>HR</h1>
+      <div className=' w-[40rem] rounded-lg overflow-hidden shadow-lg relative '>
+          <Webcam   videoConstraints={{ facingMode: "user" }} />
+          <canvas ref={canvasRef} className="absolute top-0 left-0" />
+    </div> 
+  </div>
+
+    <div>
+       <h1  className='text-white text-center'>Job Seeker</h1>
+    <div className=' w-[40rem] rounded-lg overflow-hidden shadow-lg relative '>
+          <Webcam  ref={webcamRef} videoConstraints={{ facingMode: "user" }} />
+          <canvas ref={canvasRef} className="absolute top-0 left-0" />
+    </div>
+    </div>
+</div>
+      <div>
+        <h1 className='text-2xl text-slate-100 mt-4'>Detected Emotion: {emotion}</h1>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
